07b8d2feb1157019daaa03f1cf5fdb278beef178
hv-shiva <141143387+hv-shiva@users.noreply.github.com>
2024-05-24 22:28:27 +0530

    Refactored components

--- basics-react/01-starting-project/src/App.jsx
+++ basics-react/01-starting-project/src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ["Fundamental", "Core", "Crucial"];
-
-function generateRandom(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[generateRandom(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from "./components/Headers.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
   return (
---
+++ react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear} website</p>
+        </footer>
+    );
+}
+
+export default Footer
