0ab91b010eb6819d8ae1b33f281d828a63e6e308
hv-shiva <141143387+hv-shiva@users.noreply.github.com>
2024-06-06 17:44:21 +0530

    format

--- basics-react/01-starting-project/src/components/CoreConcept.jsx
+++ basics-react/01-starting-project/src/components/CoreConcept.jsx
@@ -1,10 +1,10 @@
 
 export default function CoreConcept(props) {
-    return (
-      <li>
-        <img src={props.image} alt={props.title} />
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-      </li>
-    );
-  }
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
