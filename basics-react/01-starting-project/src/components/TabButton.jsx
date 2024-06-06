dc309fbe996ac6dfee487166f292646ff7d35bfc
hv-shiva <141143387+hv-shiva@users.noreply.github.com>
2024-06-06 17:22:23 +0530

    Dynamic styling

--- basics-react/01-starting-project/src/components/TabButton.jsx
+++ basics-react/01-starting-project/src/components/TabButton.jsx
@@ -1,9 +1,9 @@
-function TabButton({ children, onSelect }) {
+function TabButton({ children, onSelect, isSelected }) {
   console.log('TABBUTTION Component Rendered');
 
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
     </li>
   );
 }
