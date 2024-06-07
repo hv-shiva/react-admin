2980da05080ca196ddaba3a4c28ac343e4d7a1c3
hv-shiva <141143387+hv-shiva@users.noreply.github.com>
2024-06-07 16:39:48 +0530

    react updates

---
+++ basics-react/07-tic-tac-toe/src/components/player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    // setIsEditing(!isEditing); // => scheduled values to true
+    // setIsEditing(!isEditing); // => false
+    setIsEditing((editing) => !editing);
+  };
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
+
+export default Player;
--- react-stoic/react-pro/src/App.jsx
+++ react-stoic/react-pro/src/App.jsx
@@ -1,12 +1,15 @@
+<<<<<<< HEAD
 import Hello from "./components/Hello";
 import ParentComponent from "./components/ParentComponent";
 import ParentFileComponent from "./components/ParentFileComponent";
+import UseCallbackCounter from "./components/UseCallbackCounter";
 
 function App() {
   return (
     <div className="App">
       <ParentComponent />
       <ParentFileComponent />
+      <UseCallbackCounter />
     </div>
   );
 }
